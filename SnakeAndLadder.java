ca4a69c3d25cc53a0f66046e5eb7f0f446f4d20a
mohanbendegumbali <mohan0311@hotmail.com>
2023-01-12 19:31:23 +0530

    Second Commit

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,12 +2,21 @@ package com.bridgelabz.snakeladder;
 
 public class SnakeAndLadder {
 
+    private static int dieRoll(){
+    int dieValue= (int)(Math.random()*10 %6)+1;
+    return dieValue;
+}
     static int START_POSITION = 0;
 
     public static void main(String[] args) {
         // welcome to snake ladder program.
         System.out.println("Welcome to snake ladder game");
         System.out.println("Player will start from position "+START_POSITION);
+
+        int dieValue= dieRoll();
+        System.out.println("Die: "+dieValue);
+
+
     }
 }
 
