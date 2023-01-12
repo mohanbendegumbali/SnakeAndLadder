a5c9c2576782db76f6f86a21b7e7a30da6c35684
mohanbendegumbali <mohan0311@hotmail.com>
2023-01-12 20:14:26 +0530

    Third Commit

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,11 +2,35 @@ package com.bridgelabz.snakeladder;
 
 public class SnakeAndLadder {
 
-    private static int dieRoll(){
-    int dieValue= (int)(Math.random()*10 %6)+1;
-    return dieValue;
-}
     static int START_POSITION = 0;
+    static int PLAYERPOSITION = 0;
+    static final int  NO_PLAY=1;
+    static final int LADDER=2;
+    static final int SNAKE=3;
+
+    private static int dieRoll(){
+        int dieValue= (int)(Math.random()*10 %6)+1;
+        return dieValue;
+    }
+    private static int getoption(){
+        int option= (int)(Math.random()*10 %3)+1;
+        return option;
+    }
+    private static int playgame(int option, int dieValue){
+        switch(option) {
+            case NO_PLAY:
+                System.out.println("No Play");
+                break;
+            case LADDER:
+                PLAYERPOSITION = PLAYERPOSITION + dieValue;
+                break;
+            case SNAKE:
+                PLAYERPOSITION = PLAYERPOSITION - dieValue;
+        }
+
+        return PLAYERPOSITION;
+    }
+
 
     public static void main(String[] args) {
         // welcome to snake ladder program.
@@ -16,7 +40,16 @@ public class SnakeAndLadder {
         int dieValue= dieRoll();
         System.out.println("Die: "+dieValue);
 
+        int option= getoption();
+        System.out.println("option: "+option);
+
+        playgame(option,dieValue);
+
+        System.out.println("Playerposition is "+PLAYERPOSITION);
+
+
 
     }
 }
+
 
